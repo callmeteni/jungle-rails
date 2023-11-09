d835337fc33e60ec245a8bd81d4b0dd50c48c426
Slo <temiora@MacBook-Air.local>
2023-11-08 20:08:52 -0600

    intial setup for cypress

---
+++ cypress.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  "e2e": {},
+  "screenshotsFolder": "tmp/cypress_screenshots",
+  "videosFolder": "tmp/cypress_videos",
+  "trashAssetsBeforeRuns": false,
+  "projectId": "4z9utq"
+}
